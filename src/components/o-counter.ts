c800d01684be29b1aec0e01d9fc91cae5976d8da
Ruban Prakash <rubanprakash@hotmail.com>
2022-05-09 20:21:18 +0100

    add countup animation to counter component

--- src/components/o-counter.ts
+++ src/components/o-counter.ts
@@ -1,5 +1,10 @@
-import { LitElement, html, css } from 'lit';
-import { property } from 'lit/decorators.js';
+import {
+  LitElement, html, css, PropertyValueMap,
+} from 'lit';
+import {
+  property, state, query,
+} from 'lit/decorators.js';
+import { CountUp } from 'countup.js';
 import reset from './reset';
 
 export default class OCounter extends LitElement {
@@ -9,10 +14,21 @@ export default class OCounter extends LitElement {
 
   @property() unit: string = '';
 
+  @state() counterDelay: number = 500;
+
+  @query('#value') valueElem!: HTMLSpanElement;
+
   static styles = [
     reset, css`
       :host {
-        padding: 1.25rem 1.5rem;
+        background-color: var(--white);
+        height: 120px;
+        width: 155px;
+        padding: 1.00em 1.25em;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: left;
         box-shadow: 0px 34px 54px 0px rgba(6, 22, 141, 0.16);
         border-radius: 15px;
         margin: 2em;
@@ -24,14 +40,40 @@ export default class OCounter extends LitElement {
       h2 {
         font: var(--heading-l);
       }
-    `,
+      @media only screen and (max-width: 770px) {
+        :host {
+          width: 105px;
+          height: 100px;
+        }
+        h2 {
+          font: var(--heading-m);
+        }
+        p {
+          font-size: 14px;
+        }
+      }
+      `,
   ];
 
   render() {
     return html`
       <p>${this.caption}</p>
-      <h2>${this.value} ${this.unit}</h2>
+      <h2><span id="value">${this.value}</span>${this.unit}</h2>
     `;
+  }
+
+  firstUpdated() {
+    setTimeout(() => {
+      const countup = new CountUp(this.valueElem, this.value, {
+        startVal: this.value > 1000 ? 700 : 10,
+        duration: this.value > 1000 ? 3 : 4.5,
+      });
+      if (!countup.error) {
+        countup.start();
+      } else {
+        console.error(countup.error);
+      }
+    }, this.counterDelay);
   }
 }
 
